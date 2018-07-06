cb443faadf7481b51f2d2721c3aa24949c1e4a0d
NADA ZAHER <zahernada94@gmail.com>
2018-07-06 10:52:43 -0400

    WIP

---
+++ practice.js
@@ -0,0 +1,42 @@
+
+const questions =[
+    {
+        question: 'What is Nada\'s hometown?',
+        anwser1: 'Cairo, Egypt',
+        anwser2: 'New York City',
+        anwser3: 'Paris, France',
+        correct1: 'Alexandria,Egypt',
+        messageCorrect: 'Great job!',
+        messageIncorrect: 'You should know that I\'m from Alexandria!',
+    }
+    {
+        question: 'What sport did Nada play in college?',
+        anwser1: 'Soccer',
+        anwser2: 'Volleyball',
+        anwser3: 'Squash',
+        correct1: 'Tennis',
+        messageCorrect: 'Yes, I\'ve played tennis all my life.'
+        messageIncorrect: 'No, I actually played tennis all my life.'
+    }
+    {
+        question: 'What is Nada\'s favorite cuisine?',
+        anwser1: 'Japanese',
+        anwser2: 'Italian',
+        anwser3: 'Mexican',
+        correct1: 'Lebanese',
+        messageCorrect: 'Yes Lebanese is definitely my favorite, with Greek being a close second.'
+        messageIncorrect: 'No, actually Lebanese is my favorite, with Greek being a close second.'
+    }
+
+];
+questions[0].question
+
+
+function check {
+    let x;
+    for (x = 0, x < questions.length, x+=1) {
+
+    }
+    
+}
+// create 1 start and 1 check function for each 
--- script.js
+++ script.js
@@ -1,46 +1,97 @@
-
-
-// var questions = [
-// {
-//     promt: "What is Nada's hometown?"\n(a) Cairo-Egypt\n\ (b) Alexandria-Egypt\n(c)New York City\n(d) Paris", 
-//     answer: "b"
-// }
-
-// var score = 0;
-
-// for (var i=0; i < questions.length; i+=1) {
-//     var response = window.promt(questions[i].prompt);
-//     if(response === questions[i].answer){
-//         score ++;
-//         alert ("Correct!");
-//     } else {
-//         alert("Wrong!");
-//     }
-//     }
-//     alert("you got " + score + "/" + questions.length);
-
-
-function check(){
-
-    var question1 = document.question1.Q1.value;
-    var score = 0;
-
-    if (question1 === "Alexandria") {
-        score ++;
-    }
-    
-    var messages = ["Great job", "Meh", "You suck"];
+var messages = [
+    "Yes, good job!",
+    "No, you should know I'm actually from Alexandria",
+    "Correct! I've played tennis all my life.", 
+    "Nooo, I've played tennis all my life!"];
     var index;
-    if (score <1) {
-        index = 2;
-    }
-    if (score > 0 && score <2) {
-        index = 1;
-    }
-    if (score > 2) {
+    var score = 0; 
+
+function check1(){
+    var question1 = document.question1.Q1.value;
+    if (question1 === "Alexandria") {
+      score ++;
         index = 0;
     }
+    else if (question1 !== "Alexandria") {
+        index = 1;
+    }
+
     document.getElementById("message").innerHTML = messages[index];
     document.getElementById("after_submit").style.visibility = "visible";
     document.getElementById("score_tracker").innerHTML = "You got " + score + " correct.";
+}
+
+function check2(){
+    var question2 = document.question2.Q2.value;
+    
+    if (question2 === "Tennis") {
+     score ++;
+        index = 2;
+    }
+    else if (question2 !== "Tennis") {
+        index = 3; 
+    }
+
+    document.getElementById("message").innerHTML = messages[index];
+    document.getElementById("after_submit").style.visibility = "visible";
+    document.getElementById("score_tracker").innerHTML = "You got " + score + " correct.";
+}
+
+function check3(){
+    var question3 = document.question3.Q3.value;
+
+    if (question3 === "Lebanese") {
+      score ++; 
+        index = 0;
+    }
+    else if (question3 !== "Lebanese") {
+        index = 1;
+    }
+
+    document.getElementById("message").innerHTML = messages[index];
+    document.getElementById("after_submit").style.visibility = "visible";
+    document.getElementById("score_tracker").innerHTML = "You got " + score + " correct.";
+}
+
+    function start(){ 
+        document.getElementById("quiz1").style.display = "block";
+        document.querySelector("#quiz2").style.display = "none";
+        document.querySelector("#after_submit").style.display = "none";
+    }
+    function titleClear(){
+        document.querySelector(".title").style.display = "none";
+        document.querySelector(".line").style.display = "none";
+        document.querySelector(".intro").style.display = "none";
+        document.querySelector(".start").style.display = "none";
+        document.querySelector(".extra").style.display = "block";
+
+    }  
+
+    function start2 (){
+        document.querySelector("#quiz2").style.display = "block";
+        document.querySelector("#quiz1").style.display = "none";
+        document.querySelector("#after_submit").style.display = "none";
+
+    }
+
+    function start3 (){
+        document.querySelector("#quiz3").style.display = "block";
+        document.querySelector("#quiz2").style.display = "none";
+        document.querySelector("#after_submit").style.display = "none";
+
+    }
+
+    function move() {
+        var elem = document.getElementById("myBar"); 
+        var width = 10;
+        var id = setInterval(frame, 10);
+        function frame() {
+            if (width >= 100) {
+                clearInterval(id);
+            } else {
+                width++; 
+                elem.style.width = width + '%'; 
+                elem.innerHTML = width * 1 + '%';
+            }
+        }
     }

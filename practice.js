46958b785f9b85c59a4c159963bb1d1db20359f1
NADA ZAHER <zahernada94@gmail.com>
2018-07-09 16:58:17 -0400

    new changes WIP

--- practice.js
+++ practice.js
@@ -1,88 +1,298 @@
 document.getElementById("start").addEventListener("click", both)
 document.getElementById("buttonCA").addEventListener("click", checkAnswer) 
-document.getElementById("buttonNQ").addEventListener("click", start) 
+
+document.getElementById("buttonCA").addEventListener("click", function(event){
+    event.preventDefault()
+    nextQuestion()
+    getInput();
+
+})
+
+const clicked = document.querySelectorAll('.answer');
 
 let score = 0;
-let index;
-// const messages = [
-//     "Yes, good job!",
-//     "No, you should know I'm actually from Alexandria",
-//     "Correct! I've played tennis all my life.", 
-//     "Nooo, I've played tennis all my life!"];
+let global_index;
+
+function nextQuestion(){
+    global_index++;
+    askQuestion(global_index);
+    // countDown(10, "status");
+    document.getElementById("after_submit").style.display = "none";
+
+}
+
+
+function countDown(secs, elem) {
+    console.log("timer started")
+    var element = document.getElementById("status");
+    element.innerHTML = secs + " secs";
+    if(secs < 1) {
+        clearTimeout(timer);
+        element.innerHTML = '<h4>Time\'s Up!</h4>';
+    }
+    secs--;
+    var timer = setTimeout('countDown('+secs+',"'+elem+'")',1000);
+}
+
 const array =[
     {
         question: 'What is Nada\'s hometown?',
-        answer1: 'Cairo, Egypt', 
-        answer2: 'New York City',
-        answer3: 'Paris, France',
-        correct1: 'Alexandria,Egypt',
-        messageCorrect: 'Great job!',
-        messageIncorrect: 'You should know that I\'m from Alexandria!'
+        answers:[
+            {'text':'Cairo, Egypt',
+            'image': 'https://st3.depositphotos.com/2115371/18317/v/600/depositphotos_183170910-stock-video-cairo-egypt-december-2017-aerial.jpg',
+            'correct': false
+            },
+            {'text': 'New York City',
+            'image': 'https://www.housingwire.com/ext/resources/images/editorial/A-New-Big-Images/states/New-York-City.jpg?1453402066',
+            'correct': false
+            },
+            {'text': 'Paris, France',
+            'image': 'https://www.tabularaisa.com/wp-content/uploads/2015/12/eiffel.jpg',
+            'correct': false
+            },
+            {'text': 'Alexandria,Egypt',
+            'image': 'https://www.planetware.com/photos-large/EGY/egypt-alexandria-corniche.jpg',
+            'correct': true
+            }
+        ],
+        messageCorrect: 'Great - you\'re off to a good start.',
+        messageIncorrect: 'You should know that I\'m from Alexandria!',
     },
     {
         question: 'What sport did Nada play in college?',
-        answer1: 'Soccer',
-        answer2: 'Volleyball',
-        answer3: 'Squash',
-        correct1: 'Tennis',
+        answers:[
+            {'text':'Soccer',
+            'image': 'https://www.thenation.com/wp-content/uploads/2018/04/soccer-ball-ss-img.jpg',
+            'correct': false
+            },
+            {'text': 'Volleyball',
+            'image': 'https://image.mlive.com/home/mlive-media/width620/img/sports_impact/photo/grand-haven-volleyball-invitational048-9e2ef134bc63f80f.jpg',
+            'correct': false
+            },
+            {'text': 'Squash',
+            'image': 'https://vid.alarabiya.net/images/2016/12/22/eaeff1a1-c63c-4556-b584-e2bd6bca234d/eaeff1a1-c63c-4556-b584-e2bd6bca234d_16x9_788x442.jpg',
+            'correct': false
+            },
+            {'text': 'Tennis',
+            'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5mAvdN5PZOXnZA3kNZjC4Gtt2uW9QnVOPMvj3WwjYaiDo64VN',
+            'correct': true
+            }
+        ],
         messageCorrect: 'Yes, I\'ve played tennis all my life.',
         messageIncorrect: 'No, I actually played tennis all my life.'
     },
     {
         question: 'What is Nada\'s favorite cuisine?',
-        answer1: 'Japanese',
-        answer2: 'Italian',
-        answer3: 'Mexican',
-        correct1: 'Lebanese',
+        answers:[
+            {'text':'Japanese',
+            'image': 'http://cdn.lamag.com/wp-content/uploads/sites/6/2017/08/MAIN-2-1.jpg',
+            'correct': false
+            },
+            {'text': 'Italian',
+            'image': 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2015/8/7/1/FN_Italian-Favorites-Opener_s4x3.jpg.rend.hgtvcom.966.725.suffix/1438990067345.jpeg',
+            'correct': false
+            },
+            {'text': 'Mexican',
+            'image': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGBL6NhhVp3Q8D0zad9myzWZYxAY8FTfCqmeOoTABQRvtQ9-2r0Q',
+            'correct': false
+            },
+            {'text': 'Lebanese',
+            'image': 'http://www.terracerestaurantandlounge.com/wp-content/uploads/2017/02/bigstock-Arabic-Dishes-And-Meze-167170001-e1488315821840.jpg',
+            'correct': true
+            }
+        ],
         messageCorrect: 'Yes Lebanese is definitely my favorite, with Greek being a close second.',
         messageIncorrect: 'No, actually Lebanese is my favorite, with Greek being a close second.'
     },
     {
         question: 'What is Nada\'s favorite hobby?',
-        answer1: 'Watching makeup tutorials',
-        answer2: 'Reading comics',
-        answer3: 'Attending concerts',
-        correct1: 'Playing volleyball',
-        messageCorrect: 'Yes Lebanese is definitely my favorite, with Greek being a close second.',
-        messageIncorrect: 'No, actually Lebanese is my favorite, with Greek being a close second.'
+        answers:[
+            {'text':'Watching makeup tutorials',
+            'image': 'https://i.ytimg.com/vi/WZerlq1RYUQ/maxresdefault.jpg',
+            'correct': false
+            },
+            {'text': 'Reading comics',
+            'image': 'http://blog.modis.com/wp-content/uploads/2016/06/man-reading-comic-blog-300x214.jpg',
+            'correct': false
+            },
+            {'text': 'Attending concerts',
+            'image': 'http://1ryzas42x65e2oosia40bgli.wpengine.netdna-cdn.com/wp-content/uploads/2016/12/concert.jpg',
+            'correct': false
+            },
+            {'text': 'Playing volleyball',
+            'image': 'https://i.ytimg.com/vi/KelqRvBudYo/maxresdefault.jpg',
+            'correct': true
+            }
+        ],
+        messageCorrect: 'Yes, you know me pretty well. I can play volleyball for hours.',
+        messageIncorrect: 'No, my favorite hobby is playing volleyball - preferably beach.'
+    },
+    {
+        question: 'What is Nada\'s favorite movie?',
+        answers:[
+            {'text':'Lord of the Rings',
+            'image': 'https://vignette.wikia.nocookie.net/lotr/images/8/87/Ringstrilogyposter.jpg/revision/latest?cb=20070806215413',
+            'correct': false
+            },
+            {'text': 'The Proposal',
+            'image': 'https://secure-media.hotstar.com/r1/thumbs/PCTV/19/1660010119/PCTV-1660010119-hcdl.jpg',
+            'correct': false
+            },
+            {'text': 'Titanic',
+            'image': 'https://usercontent1.hubstatic.com/13810550_f520.jpg',
+            },
+            {'text': 'Invictus',
+            'image': 'https://resizing.flixster.com/kVNrR30aHtSFkj0jUr4_Rq5XMbY=/300x300/v1.bTsxMTIzOTQ3MjtqOzE3ODE1OzEyMDA7MTA1MDsxNDAw',
+            'correct': true
+            }
+        ],
+        messageCorrect: 'Yes, sports movies are my favorite.',
+        messageIncorrect: 'No, my favorite type of movies are sports movies so no wonder Invictus is the answer here.'
+    },
+    {
+        question: 'What does Nada bake best?',
+        answers:[
+            {'text':'Carrot Cake',
+            'image': 'https://www.inspiredtaste.net/wp-content/uploads/2017/07/Carrot-Cake-Recipe-4-1200.jpg',
+            'correct': false
+            },
+            {'text': 'Baklava',
+            'image': 'https://assets.marthastewart.com/styles/wmax-750/d43/baklava-mbak910-greece-1684-v2-d113210-0118/baklava-mbak910-greece-1684-v2-d113210-0118_horiz.jpg?itok=QkYIpltW',
+            'correct': false
+            },
+            {'text': 'Cupckaes',
+            'image': 'https://www.justataste.com/wp-content/uploads/2016/02/vanilla-bean-cupcakes-buttercream-frosting-recipe.jpg',
+            'correct': false
+            },
+            {'text': 'Chocolate Chip Cookies',
+            'image': 'https://belleofthekitchen.com/wp-content/uploads/2018/02/caramel-chocolate-chip-cookies-square-500x500.jpg',
+            'correct': true
+            }
+        ],
+        messageCorrect: 'Yes, my go-to is chocolate chip cookies',
+        messageIncorrect: 'No, I guess this could be tricky, but my go-to is chocolate chip cookies.'
+    },
+    {
+        question: 'What is Nada\'s favorite music genre?',
+        answers:[
+            {'text':'Hip-Hop',
+            'image': 'http://www.iamtheindustry.com/wp-content/uploads/2017/07/6361500301858884612038540517_Hip-Hop.jpg',
+            'correct': false
+            },
+            {'text': 'Classical',
+            'image': 'http://mediad.publicbroadcasting.net/p/kunr/files/styles/x_large/public/201408/Classical-Music-Wallpaper.jpg',
+            'correct': false
+            },
+            {'text': 'Reggae',
+            'image': 'http://thebestofreggae.com/wp-content/uploads/2015/09/history-of-reggae-music.jpg',
+            'correct': false
+            },
+            {'text': 'Latin',
+            'image': 'http://www.mycrowmax.com/wp-content/uploads/2013/08/latin-songs.jpg',
+            'correct': true
+            }
+        ],
+        messageCorrect: 'Yes, Latin music always puts me in a good mood and I also love dancing salsa.',
+        messageIncorrect: 'No, I actually prefer Latin music and the dancing vibe that comes with it.'
+    },
+    {
+        question: 'What is Nada\'s favorite snack?',
+        answers:[
+            {'text':'Kit Kat',
+            'image': 'https://cms.qz.com/wp-content/uploads/2017/05/kitkat.jpg?quality=80&strip=all&w=4000',
+            'correct': false
+            },
+            {'text': 'Oreos',
+            'image': 'http://static1.uk.businessinsider.com/image/54d502e0dd0895821b8b4641-480/oreos.jpg',
+            'correct': false
+            },
+            {'text': 'Gummy Bears',
+            'image': 'https://images.wisegeek.com/gummy-bears.jpg',
+            'correct': false
+            },
+            {'text': 'Sweet Potato Chips',
+            'image': 'http://www.terrachips.com/wp-content/uploads/2017/06/sweet-potato-chips.png',
+            'correct': true
+            }
+        ],
+        messageCorrect: 'Yes, I\'m impressed since I like all these snacks.',
+        messageIncorrect: 'No, sorry this was a tough one since I like all 4, but Sweet Potato Chips are my favorite.'
     }
 ];
 
+
+/*asks question*/
 function askQuestion (index) {
     var quiz = document.querySelector('#quiz');
     var questions = document.querySelector('p.questions');
-    questions.innerText = array[index].question;
-    var answr1 = document.querySelector('#mcp1');
-    answr1.innerText = array[index].answer1;
-    var answr2 = document.querySelector('#mcp2');
-    answr2.innerText = array[index].answer2;
-    var answr3 = document.querySelector('#mcp3');
-    answr3.innerText = array[index].answer3;
-    var answr4 = document.querySelector('#mcp4');
-    answr4.innerText = array[index].correct1;
-// console.log(answer)
+    questions.innerText = array[index].question; 
+
+    global_index = index //index
+
+    var shuffleAnswers = shuffle(array[index].answers);
+    console.log(shuffleAnswers);
+
+    for(var i = 0; i < shuffleAnswers.length; i++) {
+        var a = document.querySelector(`#mcp-p${i+1}`);
+        console.log(shuffleAnswers[i].image)
+        var b = document.getElementById(`mcp-p${i+1}`);
+        b.src = shuffleAnswers[i].image;
+        a.innerText = shuffleAnswers[i].text;
+        console.log('B', b)
+    }
     
+    // var answr1 = document.querySelector('#mcp-p1');
+    // answr1.innerText = array[index].answers[shuffled_questions[0]].key;
+    // answer1.src = array[index].image1;
+    // var answr2 = document.querySelector('#mcp-p2');
+    // answr2.innerText = array[index].answers[shuffled_questions[1]];
+    // var answr3 = document.querySelector('#mcp-p3');
+    // answr3.innerText = array[index].answers[shuffled_questions[2]];
+    // var answr4 = document.querySelector('#mcp-p4');
+    // answr4.innerText = array[index].answers[shuffled_questions[3]];
+
+    // var answr1 = document.querySelector('#mcp1');
+    // answr1.innerText = array[index].answers[shuffled_questions[0]];
+    // var answr2 = document.querySelector('#mcp2');
+    // answr2.innerText = array[index].answers[shuffled_questions[1]];
+    // var answr3 = document.querySelector('#mcp3');
+    // answr3.innerText = array[index].answers[shuffled_questions[2]];
+    // var answr4 = document.querySelector('#mcp4');
+    // answr4.innerText = array[index].answers[shuffled_questions[3]];
+
 }
 
-// askQuestion(0);
-
-function checkAnswer () {
-let x;
-    for (x = 0; x < question.length; x+=1) {
-        askQuestion(questions[i]);
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
     }
-    var correctAnswer = prompt(question[0], '');
-    if (correctAnswer === array[i].correct1) {
-        alert(array[i].messageCorrect);
-        score ++;
-    } else {
-        alert(array[i].messageIncorrect);
-    }
+    return a;
+}
 
-    document.getElementById("after_submit").style.visibility = "visible";
+
+
+    function checkAnswer(){
+        trueAnswer = returnCorrectAnswer();
+        userAnswer = getInput();
+        if(userAnswer === trueAnswer){
+            alert(array[global_index].messageCorrect);
+            score ++;
+        }
+        else{
+            alert(array[global_index].messageIncorrect);
+        }
+    
+    // document.getElementById("message").innerHTML = messages[global_index];
+    document.getElementById("after_submit").style.display = "block";
     document.getElementById("score_tracker").innerHTML = "You got " + score + " correct.";
+
+
 }
-    // document.getElementById("message").innerHTML = messages[index];  
+
 
 
 function start(){ 
@@ -106,38 +316,44 @@ function both (){
     start();
     askQuestion(0)
     getInput();
+    // countDown(10, "status");
+    
 }
 
-function runGame (){
-    for (x = 0; x < question.length; x+=1) {
-        askQuestion(x);
-    }
-}
-    //show question & answers
-    //get input
+// function runGame (){
+//     for (x = 0; x < question.length; x+=1) {
+//         askQuestion(x);
+//     }
+// }
 function getInput(){
-    let userAnswer;
-    const clicked = document.querySelectorAll('.answer');
     const input1 = clicked[0].checked;
     const input2 = clicked[1].checked;
     const input3 = clicked[2].checked;
     const input4 = clicked[3].checked;
 
-    while(!userAnswer){
-           if (input1 === true) {
-            userAnswer = clicked[0].value
-    } else if (input2 === true ) {
-            userAnswer = clicked[1].value
-    } else if (input3 === true) {
-            userAnswer = clicked[2].value
-    } else if (input4 === true) {
-        userAnswer = clicked[3].value
+
+
+    for(var x = 0; x < clicked.length; x++){
+
+        if(clicked[x].checked === true){
+            //something is wrong here below
+            let userAnswer = clicked[x].value;
+            console.log(userAnswer);
+
+        }
+    // return userAnswer;
+    
+}
+}
+
+function returnCorrectAnswer(){
+    const answers = array[global_index].answers    
+    for(var x = 0; x < answers.length; x++){
+        if(answers[x].correct === true){
+            correctAnswer = answers[x].text;
+        }
     }
-    break
-    console.log(userAnswer)
-    //might need to go back to FOR looop
+    return correctAnswer;
 }
-}
-    //check answer
-    //give score and comment
-    //move to next question until game ends
+
+
